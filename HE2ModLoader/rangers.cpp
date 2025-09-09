b01f836c7aab72d6c185f5c0ec31642a5ad9f70d
Ashrindy <47925123+Ashrindy@users.noreply.github.com>
2025-09-09 18:14:10 +0200

    Update HE2ModLoader/rangers.cpp
    Co-authored-by: SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>

--- HE2ModLoader/rangers.cpp
+++ HE2ModLoader/rangers.cpp
@@ -9,7 +9,7 @@
 
 DEFINE_SIGSCAN(FileSystemNativeLocal_OpenFileForReadingByPath, "\x48\x89\x5C\x24\x08\x48\x89\x74\x24\x10\x57\x48\x81\xEC\xC0\x00\x00\x00\x48\x89\xD3\x48\x89\xCE\xE8\x00\x00\x00\x00\x48\x89\xC1\xE8\x00\x00\x00\x00\x48\x8D\x4C\x24\x38", "xxxxxxxxxxxxxxx???xxxxxxx????xxxx????xxxxx")
 
-HOOK(void*, __fastcall, FileSystemNativeLocal_OpenFileForReadingByPath, _aFileSystemNativeLocal_OpenFileForReadingByPath, void* a1, char* path)
+HOOK(void*, __fastcall, FileSystemNativeLocal_OpenFileForReadingByPath, _aFileSystemNativeLocal_OpenFileForReadingByPath, void* a1, const char* path)
 {
     DWORD attributes = -1;
     for (auto& value : ReplaceDirs)
@@ -19,9 +19,9 @@ HOOK(void*, __fastcall, FileSystemNativeLocal_OpenFileForReadingByPath, _aFileSy
         attributes = GetFileAttributesA(filePath.c_str());
         if (attributes != -1)
         {
-            strcpy(path, filePath.c_str());
-            PrintInfo("Loading File: %s", path);
-            break;
+            auto newPath = filePath.c_str();
+            PrintInfo("Loading File: %s", newPath);
+            return originalFileSystemNativeLocal_OpenFileForReadingByPath(a1, newPath);
         }
     }
 

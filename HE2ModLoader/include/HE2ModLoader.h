5183861fa246429aef4513112ce49298304c04a0
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2025-09-21 04:26:56 +1000

    Bump version number

--- HE2ModLoader/include/HE2ModLoader.h
+++ HE2ModLoader/include/HE2ModLoader.h
@@ -6,7 +6,7 @@
 #include <Shlwapi.h>
 
 #define PATH_LIMIT 0x400
-#define ML_VERSION "1.3.3"
+#define ML_VERSION "1.4.0"
 
 #define DataPointer(type, name, address) \
 	static type &name = *(type *)address
